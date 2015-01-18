f70b5b07766e71296457396c15a48ceb774a1dd2
sneubig <neubig.stephen@gmail.com>
2015-01-18 13:50:00 -0500

    added headroom.js script

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	// grab an element
+	var myElement = document.querySelector("header");
+	// construct an instance of Headroom, passing the element
+	var headroom  = new Headroom(myElement);
+	// initialise
+	headroom.init(); 
+});
